040810e4e23c269fa4c197913c1cd2ff511cfea8
Michael Good <Michael@Michaels-MacBook-Pro-2.local>
2018-11-08 16:22:39 -0500

    Adding in a new line to test travis

--- server.py
+++ server.py
@@ -2,10 +2,15 @@ from flask import request, Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/hello/<name>", methods = ["GET"])
 def hello(name):
     greeting = "Hello {}!".format(name)
     return jsonify(greeting)
+
+@app.route("/api/new_patient", methods = ["POST"])
+def new_patient():
+    # For future Michael
 
 
 if __name__ == "__main__":
