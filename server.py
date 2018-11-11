c225ea27ed208c87575709eaf9e87237e36ece02
Michael Good <Michael@Michaels-MacBook-Pro.local>
2018-11-11 10:01:00 -0500

    Added in all the the method titles to be added later

--- server.py
+++ server.py
@@ -8,9 +8,36 @@ def hello(name):
     greeting = "Hello {}!".format(name)
     return jsonify(greeting)
 
+
 @app.route("/api/new_patient", methods = ["POST"])
 def new_patient():
-    # For future Michael
+    # Places a new patient into the database
+
+
+@app.route("/api/heart_rate", methods = ["POST"])
+def heart_rate():
+    # Sets the current heart rate for a given patient (also needs a time?)
+
+
+@app.route("/api/status/<patient_id>", methods = ["GET"])
+def status(patient_id):
+    # Tells whether the patient is tachycardic or not and gives
+    # the time of the previous recording
+
+
+@app.route("/api/heart_rate/<patient_id>", methods = ["GET"])
+def heart_rate_full(patient_id):
+    # Returns all previous heart rate measurements for the patient
+
+
+@app.route("/api/heart_rate/average/<patient_id>", methods = ["GET"])
+def heart_rate_average(patient_id):
+    # Gives the average of all of the patient's HR data
+
+
+@app.route("/api/heart_rate/interval_average", methods = ["POST"])
+def interval_average():
+    # Gives average heart rate since the given time
 
 
 if __name__ == "__main__":
